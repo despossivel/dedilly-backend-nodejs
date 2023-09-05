d3f42ee9cebe697e57e436b80ce04fda5fd12741
Matheus Henrique Sousa Brito <matheus.henriquesousabrito@einstein.br>
2023-09-05 10:51:05 -0300

    feat: add container and k8s files

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from "express";
+import cors from 'cors'
 import {
     geocodingController
 } from "./src/controllers/index.js"
 
+
 const port = 3033,
     app = express();
 
+    app.use(cors())
 app.get('/', (req, res) => res.send('Olá, mundo!'));
 app.get('/geocoding/:address', geocodingController);
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
